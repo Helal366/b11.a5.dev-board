5cd87450c5e83bbb265d816c000b29e0f6c85395
Helal366 <helal56876@gmail.com>
2025-02-28 23:34:21 +0600

    nav bar section complete. left side big section complete. body background javascript complete.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
-const bgColors=["#f4f7ff","#ade8dd", "#e7f8d7", "#c8e3cd", "#eba6af", "#e1a4b6", "#d3ad9f", "#bae6f4", "#d5cbf5"];
+/*body background color change */
+const bgColors=["#f4f7ff","#ade8dd", "#e7f8d7", "#c8e3cd", "#eba6af", "#bae6f4", "#d3ad9f", "#e1a4b6", "#abcdc5", "#d5cbf5"];
 let bgColorsIndex=0;
 document.getElementById("body-bg-change").addEventListener("click", function(){
     document.body.style.backgroundColor=bgColors[bgColorsIndex];
@@ -11,3 +12,20 @@ document.getElementById("body-bg-change").addEventListener("click", function(){
         }       
     }
 })
+/* main section top right part day determine */
+const fullDateToday=new Date();
+const dayToday=fullDateToday.getDay();
+const allDaysOfWeek=["Sun ,", "Mon ,", "Tues ,", "Wed ,", "Thu ,", "Fri ,", "Sat ,"];
+document.getElementById("day").innerText=allDaysOfWeek[dayToday];
+
+/* main section top right part date determine */
+// month
+const monthToday=fullDateToday.getMonth();
+const allMonth=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+document.getElementById("month").innerText=allMonth[monthToday];
+// date
+const dateToday=fullDateToday.getDate();
+document.getElementById("date").innerText=dateToday;
+// year
+const yearToday=fullDateToday.getFullYear();
+document.getElementById("year").innerText=yearToday;
