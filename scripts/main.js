31bfe4d888c6e622c8ddf3903ee30aec5e61efac
Helal366 <helal56876@gmail.com>
2025-03-01 03:32:01 +0600

    javascript for add history and remove history complete.

--- scripts/main.js
+++ scripts/main.js
@@ -30,31 +30,64 @@ document.getElementById("date").innerText=dateToday;
 // year
 const yearToday=fullDateToday.getFullYear();
 document.getElementById("year").innerText=yearToday;
+// time
+const hourToday=fullDateToday.getHours();
+const minuteToday=fullDateToday.getMinutes();
+const secondToday=fullDateToday.getSeconds();
+let currentHour=hourToday;
+if(hourToday>12){
+ currentHour=hourToday-12;
+ ampm="PM";
+}
+else{
+ currentHour=hourToday;
+ ampm="AM";
+}
+let currentTime=currentHour+":"+minuteToday+":"+secondToday+" "+ampm
 // -------------------------------------------------------------------------------------------
 
-/* decrease and increase number after click */
+/* decrease and increase number after click, alert, add history */
 const completeButtons=document.querySelectorAll(".complete");
 for(let completeButton of completeButtons ){
     completeButton.addEventListener("click", function(){
-        const id=completeButton.id;
-        const clickedButton=document.getElementById(id);
-        clickedButton.style.backgroundColor="#dfe0e9";
-        clickedButton.style.color="#cfd0d9";
-        let decreaseNumber=parseInt(document.getElementById("number-to-decrease").innerText);
-        let increaseNumber=parseInt(document.getElementById("number-to-increase").innerText);
-        if(!clickedButton.classList.contains("disabled")){
-            decreaseNumber=decreaseNumber-1;
-            console.log(decreaseNumber);
-            document.getElementById("number-to-decrease").innerText=decreaseNumber;
-            increaseNumber=increaseNumber+1;
-            console.log(increaseNumber);
-            document.getElementById("number-to-increase").innerText=increaseNumber;
-        }
-        else{
-            decreaseNumber=decreaseNumber;
-            increaseNumber=increaseNumber;
-        }
-        
-        clickedButton.classList.add("disabled");
-        })}
+    //alert
+    alert("Board updated Successfully.") 
+    //decrease increase
+    const id=completeButton.id;  
+    const clickedButton=document.getElementById(id);
+    clickedButton.style.backgroundColor="#dfe0e9";
+    clickedButton.style.color="#cfd0d9";
+    let decreaseNumber=parseInt(document.getElementById("number-to-decrease").innerText);
+    let increaseNumber=parseInt(document.getElementById("number-to-increase").innerText);
+    if(!clickedButton.classList.contains("disabled")){
+        decreaseNumber=decreaseNumber-1;
+        document.getElementById("number-to-decrease").innerText=decreaseNumber;
+        increaseNumber=increaseNumber+1;
+        document.getElementById("number-to-increase").innerText=increaseNumber;
+    }
+    else{
+        decreaseNumber=decreaseNumber;
+        increaseNumber=increaseNumber;
+    }
+    // add history
+    const titleParagraph=document.getElementById(id+"-title").innerText; 
+    const historyContainer=document.getElementById("history-container");
+    const historyAdd=document.createElement("p");
+    historyAdd.style.borderRadius="12px";
+    historyAdd.style.backgroundColor="#f4f7ff";
+    historyAdd.style.padding="10px 8px";
+    historyAdd.style.marginBottom="20px";
+
+    historyAdd.innerHTML=`
+    <span>You have completed the task ${titleParagraph} at ${currentTime}</span>
+    `
+    historyContainer.appendChild(historyAdd);
+    clickedButton.classList.add("disabled");
+})}
+
+/* clear history*/
+document.getElementById("clear-history").addEventListener("click", function(){
+    document.getElementById("history-container").innerHTML="";
+})
+
         
