24c031697e0b6946bbb504ea5cc7ce5e335bf170
Helal366 <helal56876@gmail.com>
2025-03-02 13:54:14 +0600

    Blog.html revised.

--- scripts/blog.js
+++ scripts/blog.js
@@ -3,119 +3,72 @@ document.getElementById("back").addEventListener("click", function(){
 })
 
 
+const parentIdOne="#delegate-html"
+const innerCodeOne=`<div id="fruit-div">
+                     <button class="fruit cursor-pointer btn" id="one">Apple</button>
+                     <button class="fruit cursor-pointer btn" id="two">Banana</button>
+                     <button class="fruit cursor-pointer btn" id="three">Mango</button>
+                     <button class="fruit cursor-pointer btn" id="four">Orange</button>
+                     </div>`
+myInsertDiv(parentIdOne, innerCodeOne);
 
-const parentDivOne=document.querySelector("#delegate-html");
-const newDivOne=document.createElement("div");
-const htmlCodeOne=`<div id="fruit-div">
-                <button class="fruit cursor-pointer btn" id="one">Apple</button>
-                <button class="fruit cursor-pointer btn" id="two">Banana</button>
-                <button class="fruit cursor-pointer btn" id="three">Mango</button>
-                <button class="fruit cursor-pointer btn" id="four">Orange</button>
-            </div>`
-newDivOne.innerText=htmlCodeOne;
-parentDivOne.appendChild(newDivOne);
+const parentIdTwo="#child-handler"
+const innerCodeTwo=`let fruits=document.querySelectorAll(".fruit");
+                    for(let fruit of fruits){
+                    fruit.addEventListener("click", function(event){
+                    let text=event.target.innerText;
+                    alert(text);
+                    })}`
+myInsertDiv(parentIdTwo, innerCodeTwo);
 
+const parentIdThree="#parent-handler"
+const innerCodeThree=`let fruitsList=document.querySelector("#fruit-div");
+                      fruitsList.addEventListener("click", function(event){
+                      let btn=event.target.tagName;
+                      if(btn='BUTTON'){
+                      alert(event.target.innerText);
+                      }})`
+myInsertDiv(parentIdThree, innerCodeThree);
 
-
-const parentDivTwo=document.querySelector("#child-handler");
-const newDivTwo=document.createElement("div");
-const codeTwo= `let fruits=document.querySelectorAll(".fruit");
-
-                for(let fruit of fruits){
-
-                fruit.addEventListener("click", function(event){
-
-                let text=event.target.innerText;
-
-                alert(text);
-
-                })}`
-newDivTwo.innerText=codeTwo;
-parentDivTwo.appendChild(newDivTwo);
-
-
-
-const parentDivThree=document.querySelector("#parent-handler");
-const newDivThree=document.createElement("div");
-const codeThree= `let fruitsList=document.querySelector("#fruit-div");
-
-                  fruitsList.addEventListener("click", function(event){
-
-                  let btn=event.target.tagName;
-
-                  if(btn='BUTTON'){
-
-                  alert(event.target.innerText);
-
-                  }})`
-newDivThree.innerText=codeThree;
-parentDivThree.appendChild(newDivThree);
-
-
-
-const parentDivFour=document.querySelector("#container");
-const newDivFour=document.createElement("div");
-const codeFour= `<div id="text-container">
-
+const parentIdFour="#container"
+const innerCodeFour=`<div id="text-container">
                         <p id="p-one">Create yourself. Be skilled.</p>
-
                         <p>Coding, Digital marketing, DevOps, Machine Learning are good skills now a days.</p>
+                        <p>HTML, CSS and Javascript are beginning path for a frontend developer.</p>        
+                    </div>`
+myInsertDiv(parentIdFour, innerCodeFour);
 
-                        <p>HTML, CSS and Javascript are beginning path for a frontend developer.</p>
-                    
-                 </div>`
-newDivFour.innerText=codeFour;
-parentDivFour.appendChild(newDivFour);
-
-
-const parentDivFive=document.querySelector("#container-two");
-const newDivFive=document.createElement("div");
-const codeFive= `const newCreateDiv=document.createElement("div");                
-                 const newCreateParagraph=document.createElement("p");
-                 `
-newDivFive.innerText=codeFive;
-parentDivFive.appendChild(newDivFive);
+const parentIdFive="#container-two"
+const innerCodeFive=`const newCreateDiv=document.createElement("div");                
+                     const newCreateParagraph=document.createElement("p");`
+myInsertDiv(parentIdFive, innerCodeFive);
 
 
-const parentDivSix=document.querySelector("#container-three");
-const newDivSix=document.createElement("div");
-const codeSix= `const textContainer=document.querySelector("#text-container");
-
-                 newCreateParagraph.innerText="Hello! This is a created paragraph."
-                 textContainer.appendChild(newCreateParagraph);
-`
-newDivSix.innerText=codeSix;
-parentDivSix.appendChild(newDivSix);
+const parentIdSix="#container-three"
+const innerCodeSix=`const textContainer=document.querySelector("#text-container");
+                    newCreateParagraph.innerText="Hello! This is a created paragraph."
+                    textContainer.appendChild(newCreateParagraph);`
+myInsertDiv(parentIdSix, innerCodeSix);
 
 
-const parentDivSeven=document.querySelector("#container-four");
-const newDivSeven=document.createElement("div");
-const codeSeven= `newCreateDiv.innerText="Hello! This is a created div."
-                  textContainer.insertBefore(newCreateDiv, textContainer.firstElementChild);`
-newDivSeven.innerText=codeSeven;
-parentDivSeven.appendChild(newDivSeven);
+const parentIdSeven="#container-four"
+const innerCodeSeven=`newCreateDiv.innerText="Hello! This is a created div."
+                      textContainer.insertBefore(newCreateDiv, textContainer.firstElementChild);`
+myInsertDiv(parentIdSeven, innerCodeSeven);
+
+const parentIdEight="#container-five"
+const innerCodeEight=`const removeParagraph=document.querySelector("#p-one");
+           textContainer.removeChild(removeParagraph);`
+myInsertDiv(parentIdEight, innerCodeEight);
 
 
-const parentDivEight=document.querySelector("#container-five");
-const newDivEight=document.createElement("div");
-const codeEight= `const removeParagraph=document.querySelector("#p-one");
-                  textContainer.removeChild(removeParagraph);`
-newDivEight.innerText=codeEight;
-parentDivEight.appendChild(newDivEight);
+function myInsertDiv(parentId, code){
+const parentDiv=document.querySelector(parentId);
+const newDiv=document.createElement("div");
+newDiv.innerText=code;
+parentDiv.appendChild(newDiv);
+}
 
 
 
-const textContainer=document.querySelector("#text-container");
-console.log(textContainer);
-
-const newCreateDiv=document.createElement("div");
-newCreateDiv.innerText="Hello! This is a created div."
-textContainer.insertBefore(newCreateDiv, textContainer.firstElementChild);
-
-const newCreateParagraph=document.createElement("p");
-newCreateParagraph.innerText="Hello! This is a created paragraph."
-textContainer.appendChild(newCreateParagraph);
-
-const removeParagraph=document.querySelector("#p-one");
-textContainer.removeChild(removeParagraph);
 
